a8cb08d449d2cc093d7893158645dd1847a6948e
Centeno Jose Antonio <jocecito182@gmail.com>
2023-12-30 12:32:21 -0300

    Astro first steps

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Jose Centeno Dev | Personal Portfolio Website';
+export const SITE_DESCRIPTION = '"Full Stack Developer crafting seamless digital experiences. Expert in front-end and back-end technologies. Passionate about transforming ideas into scalable and user-centric web solutions.';
